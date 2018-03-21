27c4efddaba6c51b7bbd9d5897e455f46e98d0c9
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-20 20:52:29 -0400

    created Registered component and created a basic static signup form in my app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,44 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      username: '',
+      password: ''
+    }
+    this.handleUsername = this.handleUsername.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
+  }
+  
+  handleUsername(e){
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  handlePassword(e){
+    this.setState({
+      password: e.target.value
+    });
+  }
+  
   render() {
     return (
       <div>
-        hello
+        <h1>Sign Up</h1>
+        <form>
+          <input type="text"
+                 placeholder='Email'
+                 required/>
+          <input type="password"
+                 placeholder='Password'
+                 required/>
+          <input type="text"
+                 placeholder='Username'
+                 required/>
+          <button type="submit">Sign Up</button>
+        </form>
       </div>
     );
   }
---
+++ src/Registered.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Registered extends Component {
+    render(){
+        return(
+            <div>
+                <p>Congradulations you have been registered! you may now login</p>
+                <button>Login</button>
+            </div>
+        );
+    }
+}
+
+export default Registered;
